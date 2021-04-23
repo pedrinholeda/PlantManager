758bbc1fa90d87835f18957684e76e321c502c20
Pedro Leda <pedro.henrique.leda@gmail.com>
2021-04-22 22:34:18 -0300

    constructing welcome screen

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Welcome } from './src/pages/Welcome';
+
+export default function App() {
+  return (
+    <Welcome/>
+  );
+}
+
+
