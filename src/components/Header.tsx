196f0158f3fbee260a215235b549257c8617984f
Pedro Leda <pedro.henrique.leda@gmail.com>
2021-05-17 22:01:45 -0300

    Plant Select screen

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+import {getStatusBarHeight} from 'react-native-iphone-x-helper';
+
+import userImg from '../assets/profile.jpeg';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+
+export function Header(){
+    return(
+        <View style={styles.container}>
+             <View>
+                 <Text style={styles.greeting}>Olá,</Text>
+                 <Text style={styles.username}>Pedro</Text>
+             </View>
+
+            <Image source={userImg} style={styles.image}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width:'100%',
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+        padding:20
+    },
+    image:{
+        width:70,
+        height:70,
+        borderRadius:40
+    },
+    greeting:{
+        fontSize:32,
+        color: colors.heading,
+        fontFamily:fonts.text
+
+    },
+    username:{
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight:40
+
+    }
+})
